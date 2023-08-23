6318b1577c957dd8462301966caf3907b7a55e8b
Emad Malekpour <prodemmi@gmail.com>
2023-08-23 12:20:55 +0330

    Add ui-components to admin-panel

--- packages/ui-components/src/App.vue
+++ packages/ui-components/src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  This is Ui Component
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
